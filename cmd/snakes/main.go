f1ac07a652031a37546d7e44d79a3d7ee89e383c
Dylan Hanger <21023770+dylanHanger@users.noreply.github.com>
2025-09-29 20:36:59 +0200

    feat: add icon

--- cmd/snakes/main.go
+++ cmd/snakes/main.go
@@ -1,28 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"image"
+
+	_ "embed"
 
 	"github.com/dylanHanger/snakes/pkg"
 	"github.com/dylanHanger/snakes/pkg/snakes"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
-// TODO: Fix Go's stupid fucking random behaviour that makes everything fucking non-deterministic
+//go:embed icon.png
+var Icon []byte
+
 // TODO: Scoring
 // TODO: UI (display winner, scoreboard, game over, restart, pause, resume, step buttons)
 func main() {
 	// Define command line flags
-	configFlag := flag.String("config", "config/config.yaml", "Path to config file")
+	configFlag := flag.String("config", "config.yaml", "Path to config file")
 	flag.Parse()
 
 	configPath := *configFlag
 	cfg, err := snakes.LoadConfig(configPath)
 	if err != nil {
-		fmt.Printf("Could not load config %s: %v\n", configPath, err)
+		fmt.Printf("Could not load config `%s`: %v\n", configPath, err)
 		cfg = snakes.DefaultConfig()
 	}
 	g := snakes.NewGame(cfg)
 	e := pkg.NewEbitenEngine(g)
+	img, _, _ := image.Decode(bytes.NewReader(Icon))
+	ebiten.SetWindowIcon([]image.Image{img})
 	e.Run()
 }
